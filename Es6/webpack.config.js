f1c68481e09e208857c903862f68021d96dde77e
dingxl <dingxl>
2019-05-01 08:02:01 +0800

    eslint

--- Es6/webpack.config.js
+++ Es6/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const config = {
+  mode: 'development',
   entry: './src/main.js',
   output: {
     filename: 'bundle-[hash].js',
