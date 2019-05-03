191096311a7b3fb33ac327d47b451b899c41971a
dingxl <dingxl>
2019-05-03 12:33:50 +0800

    Update pack

--- WebPack/readme.md
+++ WebPack/readme.md
@@ -2,12 +2,25 @@
 https://segmentfault.com/a/1190000006178770
 https://github.com/Val-Zhang/webpack-demo
 
-npm i -g webpack webpack-dev-server
+npm init
+
+cnpm i -g webpack
+cnpm i webpack webpack-cli webpack-dev-server -D
+cnpm i babel-core babel-loader babel-preset-env babel-preset-react -D
+
+cnpm i react react-dom -S
+cnpm i style-loader css-loader -D
+cnpm i postcss-loader autoprefixer -D
+cnpm i html-webpack-plugin -D
+cnpm i babel-plugin-react-transform react-transform-hmr -D
+
+cnpm i extract-text-webpack-plugin -D
+cnpm i clean-webpack-plugin -D
 
 cnpm install
 npm start
 
 npm run build
 
-npm install -g serve
+cnpm install -g serve
 serve -s dist -p 3000
--- WebPack/webpack.config.js
+++ WebPack/webpack.config.js
@@ -1,40 +1,56 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  entry: __dirname + "/src/index.js",
-  output: {
-    path: __dirname + "/dist",
-    filename: "index_bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 3000
-  },
-  module: {
-    rules: [{
-      test: /(\.jsx|\.js)$/,
-      use: {
-        loader: "babel-loader",
-      },
-      exclude: /node_modules/
-    }, {
-      test: /\.css$/,
-      use: [{
-        loader: "style-loader"
-      }, {
-        loader: "css-loader"
-      }, {
-        loader: "postcss-loader"
-      }]
-    }]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: __dirname + "/public/index.html"
-    })
-  ]
-}
+    entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle-[hash].js"
+    },
+
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        historyApiFallback: true,//不跳转
+        inline: true,
+        hot: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }, {
+                        loader: "postcss-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    optimization: {
+      minimize: true
+    },
+    plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+        }),
+        new webpack.HotModuleReplacementPlugin() //热加载插件
+    ],
+};
