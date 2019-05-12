5b49ab31a6e56673283fa7546457997be27596c4
dingxl <dingxl>
2019-05-12 16:11:20 +0800

    add home entry

--- ReactCli/app/src/App.js
+++ ReactCli/app/src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import './App.css';
 import Link from "react-router-dom/es/Link";
 import About from "./containers/about";
+import Home from "./containers/home";
 import Inbox from "./containers/inbox";
 import Switch from "react-router-dom/es/Switch";
 import Route from "react-router-dom/es/Route";
+import './App.css';
 
 class App extends Component {
     render() {
@@ -13,6 +14,7 @@ class App extends Component {
                 <h1>App</h1>
                 <ul>
                     <li><Link to="/About">About</Link></li>
+                    <li><Link to="/Home">Home</Link></li>
                     <li><Link to="/Inbox">Inbox</Link></li>
                     <li><Link to="/Messages/2">Message</Link></li>
                 </ul>
@@ -20,6 +22,7 @@ class App extends Component {
                 <main>
                     <Switch>
                         <Route path="/About" component={About}/>
+                        <Route path="/Home" component={Home}/>
                         <Route path="/Inbox" component={Inbox}/>
                     </Switch>
                 </main>
